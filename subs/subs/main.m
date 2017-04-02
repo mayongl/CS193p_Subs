e0ab7efce6d25f37e1203452d3acd5daed680a9b
Yonglin <mayongl@apache.org>
2017-04-02 10:57:29 +0800

    English subs for 4,5,6,11 and empty sub project.

---
+++ subs/subs/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  subs
+//
+//  Created by Yonglin Ma on 4/2/17.
+//  Copyright © 2017 Sixlivesleft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
